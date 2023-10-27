be38f604ff9ecbc165714a851848a7a07ab80213
Sagar <sagar@beanbyte.in>
2023-10-27 14:10:57 +0530

    changes in node

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const express = required()
+const express = require('express');
+const path = require('path');
+const port = 8004;
+const app = express();
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+app.get('/',function(req,res){
+    return res.render('nike');
+})
+app.listen(port,function(err){
+    if(err){
+        console.log("there is an error in running the server",err);
+
+    }
+    console.log("server running on port:",port);
+})
+
